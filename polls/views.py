08daeb5fb27db9686929206e2803cd9bad4fa097
Alex Bispo <alexbispo.artes@gmail.com>
2018-05-05 23:06:59 -0300

    Adiciona detail template.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render, get_object_or_404
 
 from .models import Question
 
@@ -12,7 +11,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("Você está vendo a questão %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("Você está vendo os resultados da questão %s" % question_id)
