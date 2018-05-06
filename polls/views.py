714408e276ae3c4c8a81748a4190eee95c27e442
Alex Bispo <alexbispo.artes@gmail.com>
2018-05-05 22:36:58 -0300

    Adiciona template index.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Olá, mundo. Você está na página inicial do polls.")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'questions': questions,
+    }
 
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("Você está vendo a questão %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Você está vendo os resultados da questão %s" % question_id)
+
+def vote(request, question_id): 
+    return HttpResponse("Você está votando na questão %s." % question_id)
